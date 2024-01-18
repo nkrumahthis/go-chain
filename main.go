c9800ee467dd44fcaac6bd417ec0ade1ab9fb525
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-18 23:18:29 +0000

    blockchain.AddBlock

--- main.go
+++ main.go
@@ -27,5 +27,11 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
 func main() {
 }
