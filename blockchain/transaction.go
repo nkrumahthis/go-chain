21afe61b4d7b3ef7dc1f11f6187681bef2bf1913
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-23 23:09:57 +0000

    transaction setid and coinbase

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
 type TxInput struct {
 	ID []byte
 	Out int
@@ -17,3 +24,29 @@ type Transaction struct {
 	Output []TxOutput
 }
 
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+	
+	encode := gob.NewEncoder(&encoded)
+	err := encode.Encode(tx)
+	Handle(err)
+
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
+
+func Coinbase(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coins to %s", to)
+	}
+
+	txin := TxInput{[]byte{}, -1, data}
+	txout := TxOutput{100, to}
+
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+	tx.SetID()
+
+	return &tx
+
+}
