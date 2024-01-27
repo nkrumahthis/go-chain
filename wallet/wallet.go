3b6eebee217c4677f01c97d489da1274d70c9250
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-27 14:46:35 +0000

    wallet module: make wallet

---
+++ wallet/wallet.go
@@ -0,0 +1,32 @@
+package blockchain
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+)
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+func NewKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+	return *private, pub
+}
+
+func MakeWallet() *Wallet {
+	private, public := NewKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
