3a2d95b8d722e731c5f786c563c9e2a2589016ad
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-27 18:56:00 +0000

    generate wallet address

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
+	"fmt"
 	"log"
 
 	"golang.org/x/crypto/ripemd160"
@@ -18,6 +19,22 @@ const (
 type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
 	PublicKey  []byte
+}
+
+func (w Wallet) Address() []byte {
+	pubHash := PublicKeyHash(w.PublicKey)
+
+	versionedHash := append([]byte{version}, pubHash...)
+	checksum := Checksum(versionedHash)
+
+	fullHash := append(versionedHash, checksum...)
+	address := Base58Decode(fullHash)
+
+	fmt.Printf("pub key: %x\n", w.PublicKey)
+	fmt.Printf("pub hash: %x\n", pubHash)
+	fmt.Printf("address: %x\n", address)
+
+	return address
 }
 
 func NewKeyPair() (ecdsa.PrivateKey, []byte) {
